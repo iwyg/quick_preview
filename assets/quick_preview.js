4af66c96ca3e7490f5f5acc1cffe2d49d2146a7a
thomas appel <mail@thomas-appel.com>
2011-06-21 14:28:05 +0200

    added preview access to all image links;refactored js: removed implied globals, made script more robust and reliable

--- assets/quick_preview.js
+++ assets/quick_preview.js
@@ -1,4 +1,8 @@
-(function($){
+(function($, global){
+	
+	var xOffset,st,qh,wh,ww,s,qw,
+		$global = $(global);
+	
 	$.fn.delay = function(time, name){
 		return this.queue(( name || "fx" ), function(){
 			var self = this;
@@ -7,52 +11,12 @@
 			}, time );
 		});
 	};
-	
-	$.fn.quickPreview = function(){
-		
-		this.hover(function(e){
-			$('body').append('<div id="quick-preview"><span></span><div id="holder" class="loading"></div></div>');
-			$('#quick-preview').fadeIn('fast');
-			var image = new Image();
-			$(image)
-				.load(function(){
-					$(this).hide();
-					$('#quick-preview span').text(this.width + "x" + this.height)
-						.fadeIn('fast').delay(3000).fadeOut('fast');
-					$('#holder')
-						.removeClass('loading')
-						.empty()
-						.css('width', this.width)
-						.css('height', this.height)
-						.append(this);
-						
-					correctPosition(e);
-					$(this).fadeIn('fast');
-				})
-				.error(function(){
-					$('#holder').text('Error loading image!');
-				})
-				.attr('src', this.href);
-		}, function(){
-			$('#quick-preview').remove();
-		});
-		
-		this.mousemove(function(e){
-			correctPosition(e);
-		});
-		
-		function correctPosition(e){
-			$('#quick-preview')
-				.css('top', ( evalTop( e.pageY ) ) + 'px')
-				.css('left', ( evalLeft( e.pageX ) ) + 'px');
-		}
-		
-		function evalTop(u){
-			xOffset = 20;
-			st = $(window).scrollTop();
-			qh = $('#quick-preview').height();
-			wh = $(window).height();
-			ww = $(window).width();
+	function evalTop(u, el){
+		xOffset = 20;
+		st = $global.scrollTop();
+		qh = el.height();
+		wh = $global.height();
+		ww = $global.width();
 			
 			s = u - st - qh / 2;
 			if ( s + qh > wh - 12 ) {
@@ -64,24 +28,94 @@
 			return s;
 		}
 		
-		function evalLeft(u){
-			s = u + xOffset;
-			qw = $('#quick-preview').width();
-			if( s + qw + 12 > ww ) {
-				if( qw + xOffset + 12 < u ) {
-					s = u - xOffset - qw - 12;
-				}
+	function evalLeft(u, el){
+		s = u + xOffset;
+		qw = el.width();
+		if( s + qw + 12 > ww ) {
+			if( qw + xOffset + 12 < u ) {
+				s = u - xOffset - qw - 12;
 			}
-			return s;
 		}
+		return s;
+	}
+	
+	function QuickPreview (){
+		this.setup();
+	}
+	
+	QuickPreview.prototype = {
+		setup: function(){
+			this.el = $('<div id="quick-preview"><span></span><div id="holder" class="loading"></div></div>');
+			
+			$('body')
+					.bind('mousemove', $.proxy(this.correctPosition, this))
+					.delegate('a','mouseenter',$.proxy(this.trigger,this))
+					.delegate('a','mouseleave',$.proxy(this.removePreview, this));				
+		},
 		
+		showPreview: function(e, src) {
+			var that = this,image,
+				holder = that.el.find('#holder');
+			
+			$('body').append(this.el)			
+			
+			this.el.fadeIn('fast');
+			
+			image = new Image();
+			$(image)
+				.load(function(){
+					$(this).hide();
+					that.el.find('span').text(this.width + "x" + this.height)
+						.fadeIn('fast').delay(3000).fadeOut('fast');
+						
+					holder
+						.removeClass('loading')
+						.empty()
+						.css('width', this.width)
+						.css('height', this.height)
+						.append(this);
+						
+					that.correctPosition(e);
+					$(this).fadeIn('fast');
+				})
+				.error(function(){
+					holder.text('Error loading image!');
+				})
+				.attr('src', src);			
+			
+		},
+		
+		removePreview: function(){
+			this.el.detach();			
+		},
+		correctPosition: function(e){
+			this.el.css('top', ( evalTop( e.pageY, this.el ) ) + 'px')				
+				.css('left', ( evalLeft( e.pageX, this.el ) ) + 'px');			
+		},
+		trigger: function(e){
+			var target = $([e.target, e.target.parentNode]).filter(function(){
+				return !!this.href && this;
+			})[0];
+			if (target.href.match(/\.(?:bmp|gif|jpe?g|png)$/i)) {
+				this.showPreview.call(this,e, target.href);
+			}
+		}
+	}
+	
+	
+	$.fn.quickPreview = function(){
+		this.data('quickpreview', new QuickPreview());
 		return this;
 	};
 	
 	$(document).ready(function(){
+		
+		$('form').quickPreview();
+		/*
 		$('.field-upload, .selectable, .field-uniqueupload').find('a').map(function(){
 			if (this.href.match(/\.(?:bmp|gif|jpe?g|png)$/i)) return this;
 		}).quickPreview();
+		*/
 	});
 	
-})(jQuery);
+}(this.jQuery, this));
